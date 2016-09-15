4ed87cac7e4c8eb3ce05d3d74a9f0142544c4186
PhilKunz <github@philkunz.com>
2016-09-15 14:45:58 +0200

    now adhering to standardjs

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="q" />
-import "typings-global";
-import q = require("q");
+import 'typings-global';
+import q = require('q');
 export declare let start: (moduleNameArg?: string, loaderLengthArg?: string) => void;
-export declare let stop: () => q.Promise<{}>;
+export declare let stop: () => q.Promise<void>;
--- ts/index.ts
+++ ts/index.ts
@@ -1,56 +1,54 @@
-import "typings-global"
-import path = require("path");
-import chalk = require("chalk");
-import q = require("q");
-import readline = require("readline");
-import childProcess = require("child_process");
-let earlyChild;
+import 'typings-global'
+import path = require('path')
+import chalk = require('chalk')
+import q = require('q')
+import childProcess = require('child_process')
+let earlyChild
 
-let doAnimation: boolean = true;
-let doText: boolean = false;
-let moduleName: string = "undefined module name";
-let startTime;
+let doAnimation: boolean = true
+let doText: boolean = false
+let moduleName: string = 'undefined module name'
+let startTime
 
-if (process.argv.indexOf("-v") != -1 || process.env.CI) {
-    doAnimation = false;
+if (process.argv.indexOf('-v') !== -1 || process.env.CI) {
+    doAnimation = false
 }
-if (process.argv.indexOf("-v") == -1 && process.env.CI) {
-    doText = true;
+if (process.argv.indexOf('-v') === -1 && process.env.CI) {
+    doText = true
 }
 
 // exports
-export let start = function (moduleNameArg: string = "", loaderLengthArg: string = "10") {
+export let start = function (moduleNameArg: string = '', loaderLengthArg: string = '10') {
 
-    startTime = Date.now();
+    startTime = Date.now()
 
-    moduleName = moduleNameArg;
+    moduleName = moduleNameArg
     if (doAnimation) {
-        earlyChild = childProcess.fork(path.join(__dirname, "early.child.js"), [], {
+        earlyChild = childProcess.fork(path.join(__dirname, 'early.child.js'), [], {
             env: {
                 moduleNameArg: moduleNameArg,
                 loaderLengthArg: loaderLengthArg,
                 CI: process.env.CI
             }
-        });
+        })
     } else if (doText) {
-        console.log(`**** starting ${chalk.green(moduleNameArg)} ****`);
+        console.log(`**** starting ${chalk.green(moduleNameArg)} ****`)
     }
-};
+}
 
-export let stop = function () {
-    let done = q.defer();
-    let endTime = Date.now();
-    let executionTime:string = ((endTime - startTime) / 1000).toString();
+export let stop = (): q.Promise<void> => {
+    let done = q.defer<void>()
+    let endTime = Date.now()
+    let executionTime: string = ((endTime - startTime) / 1000).toString()
     if (doAnimation) {
-        earlyChild.kill("SIGINT");
-        earlyChild.on("close", function () {
-            console.log(`loaded ${chalk.green(moduleName)} in ${executionTime} seconds!`);
-            done.resolve();
+        earlyChild.kill('SIGINT')
+        earlyChild.on('close', function () {
+            console.log(`loaded ${chalk.green(moduleName)} in ${executionTime} seconds!`)
+            done.resolve()
         })
     } else {
-        console.log(`... finished loading ${moduleName} in ${executionTime}`);
-        done.resolve();
+        console.log(`... finished loading ${moduleName} in ${executionTime}`)
+        done.resolve()
     }
-    return done.promise;
-};
-
+    return done.promise
+}
