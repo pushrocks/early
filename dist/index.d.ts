bfef45cab320da8c4fce3026963a00a201a952d1
PhilKunz <github@philkunz.com>
2016-09-19 00:34:38 +0200

    update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,8 @@
 /// <reference types="q" />
 import 'typings-global';
 import q = require('q');
+/**
+ * start the loading
+ */
 export declare let start: (moduleNameArg?: string, loaderLengthArg?: string) => void;
 export declare let stop: () => q.Promise<number>;
--- ts/index.ts
+++ ts/index.ts
@@ -2,36 +2,22 @@ import 'typings-global'
 import path = require('path')
 import chalk = require('chalk')
 import q = require('q')
-import childProcess = require('child_process')
-let earlyChild
 
-let doAnimation: boolean = true
+
 let doText: boolean = false
 let moduleName: string = 'undefined module name'
-let startTime
+let startTime = Date.now()
 
-if (process.argv.indexOf('-v') !== -1 || process.env.CI) {
-    doAnimation = false
-}
-if (process.argv.indexOf('-v') === -1 && process.env.CI) {
+if (process.argv.indexOf('-v') === -1) {
     doText = true
 }
 
-// exports
+/**
+ * start the loading
+ */
 export let start = function (moduleNameArg: string = '', loaderLengthArg: string = '10') {
-
-    startTime = Date.now()
-
     moduleName = moduleNameArg
-    if (doAnimation) {
-        earlyChild = childProcess.fork(path.join(__dirname, 'early.child.js'), [], {
-            env: {
-                moduleNameArg: moduleNameArg,
-                loaderLengthArg: loaderLengthArg,
-                CI: process.env.CI
-            }
-        })
-    } else if (doText) {
+    if (doText) {
         console.log(`**** starting ${chalk.green(moduleNameArg)} ****`)
     }
 }
@@ -41,15 +27,7 @@ export let stop = (): q.Promise<number> => {
     let endTime = Date.now()
     let earlyExecutionTime: number = (endTime - startTime)
     let earlyExecutionTimeString: string = (earlyExecutionTime / 1000).toString()
-    if (doAnimation) {
-        earlyChild.kill('SIGINT')
-        earlyChild.on('close', function () {
-            console.log(`loaded ${chalk.green(moduleName)} in ${earlyExecutionTimeString} seconds!`)
-            done.resolve(earlyExecutionTime)
-        })
-    } else {
-        console.log(`... finished loading ${moduleName} in ${earlyExecutionTimeString}`)
-        done.resolve(earlyExecutionTime)
-    }
+    console.log(`... finished loading ${moduleName} in ${earlyExecutionTimeString}`)
+    done.resolve(earlyExecutionTime)
     return done.promise
 }
